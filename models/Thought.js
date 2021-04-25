aae06b897b56ba5de81c3317ee67bf20cfaf6fc8
mambru <ecastro82@gmail.com>
2021-04-25 09:37:09 -0500

    added reaction schema model

--- models/Thought.js
+++ models/Thought.js
@@ -1,6 +1,34 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
 const dateFormat = require('../utils/dateFormat.js')
 
+const ReactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true
+            //280 character maximum
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        }
+    },
+    {
+        toJSON: {
+            getters: true,
+            virtuals: true
+        }
+    }
+)
 const ThoughtSchema = new Schema({
     thoughtText: {
         type: String,
@@ -16,7 +44,7 @@ const ThoughtSchema = new Schema({
         type: String,
         required: true
     },
-    reactions:[]
+    reactions:[ReactionSchema]
 },
 {
     toJSON: {
@@ -26,8 +54,12 @@ const ThoughtSchema = new Schema({
 }
 )
 
+ThoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+})
+
 const Thought = model('Thought', ThoughtSchema)
 
-//Reaction schema pending
+
 
 module.exports = Thought
